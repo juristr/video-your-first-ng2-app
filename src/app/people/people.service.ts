007a572d403eb19be128e897bba6328718886430
Juri <juri.strumpflohner@gmail.com>
2016-12-03 15:19:42 +0100

    configure production build and AoT compilation

--- src/app/people/people.service.ts
+++ src/app/people/people.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 
@@ -8,7 +9,7 @@ export class PeopleService {
 
   constructor(private http: Http) { }
 
-  fetchPeople() {
+  fetchPeople() : Observable<any> {
     return this.http
       .get('/assets/people.json')
       .map(res => res.json());
