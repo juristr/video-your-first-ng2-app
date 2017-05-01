384d047cf253dca0288c2377fe16418db8be3602
Sivabudh Umpudh <sivabudh.u@codium.co>
2017-05-01 16:03:42 +0700

    Reformatted file.

--- src/app/people/people.service.ts
+++ src/app/people/people.service.ts
@@ -1,15 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
-import 'rxjs/add/operator/map';
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class PeopleService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
-  fetchPeople() : Observable<any> {
+  fetchPeople(): Observable<any> {
     return this.http
       .get('/assets/people.json')
       .map(res => res.json());
